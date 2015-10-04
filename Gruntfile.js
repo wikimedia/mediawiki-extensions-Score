5312f238c511b03ff499a0cf7d8b814b24c6fc9c
Ed Sanders <esanders@wikimedia.org>
2015-10-04 18:16:12 +0100

    Add grunt linting tasks
    Change-Id: Ida8ec3de35d810ebb21176bf71359b16c0c29049

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,43 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-csslint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
 
 	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'js/*.js',
+				'modules/ve-score/**/*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc'
+			},
+			all: 'modules/ve-score/**/*.css'
+		},
+		watch: {
+			files: [
+				'.{jscsrc,jshintignore,jshintrc}',
+				'<%= jshint.all %>'
+			],
+			tasks: 'lint'
+		},
 		banana: {
+			options: {
+				disallowDuplicateTranslations: false
+			},
 			all: 'i18n/'
 		},
 		jsonlint: {
@@ -15,6 +48,7 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'lint', [ 'jshint', 'jscs', 'jsonlint', 'csslint', 'banana' ] );
+	grunt.registerTask( 'test', 'lint' );
 	grunt.registerTask( 'default', 'test' );
 };
