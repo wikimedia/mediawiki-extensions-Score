75c41f94f6b9f34b697c91715465514cb7c542eb
Sébastien Santoro <dereckson@espace-win.org>
2017-01-08 20:24:51 +0000

    Move ScoreException class to standalone file
    Change-Id: I76c2436cfb2aa1e1b8fac6ee52ef357224ede5c3

---
+++ ScoreException.php
@@ -0,0 +1,56 @@
+<?php
+/*
+	Score, a MediaWiki extension for rendering musical scores with LilyPond.
+	Copyright © 2011 Alexander Klauer
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	To contact the author:
+	<Graf.Zahl@gmx.net>
+	http://en.wikisource.org/wiki/User_talk:GrafZahl
+	https://github.com/TheCount/score
+
+ */
+
+/**
+ * Score exception
+ */
+class ScoreException extends Exception {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param $message Message to create error message from. Should have one $1 parameter.
+	 * @param $code int optionally, an error code.
+	 * @param $previous Exception that caused this exception.
+	 */
+	public function __construct( $message, $code = 0, Exception $previous = null ) {
+		parent::__construct( $message->inContentLanguage()->parse(), $code, $previous );
+	}
+
+	/**
+	 * Auto-renders exception as HTML error message in the wiki's content
+	 * language.
+	 *
+	 * @return string Error message HTML.
+	 */
+	public function  __toString() {
+		return Html::rawElement(
+			'div',
+			array( 'class' => 'errorbox' ),
+			$this->getMessage()
+		);
+	}
+
+}
