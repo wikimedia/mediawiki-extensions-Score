22a578b19fcb5c9269a8d8cb66b00890da80e660
Derick Alangi <alangiderick@gmail.com>
2019-04-07 11:30:51 +0100

    ScoreHooks: Remove legacy hook behavior and & on object as reference
    [[mw:Manual:Hooks#Hook_behavior_before_MediaWiki_1.22_vs_after]] attemps
    to explain this recent practice on hook behavior. Also, objects are passed
    by default as references, so no need for the &.
    
    Change-Id: Ia7fc3abd402d96474fa78a889ea190a4d469e779

--- includes/ScoreHooks.php
+++ includes/ScoreHooks.php
@@ -12,18 +12,15 @@ use Wikimedia\Purtle\RdfWriter;
 
 class ScoreHooks {
 	/**
-	 * @param Parser &$parser
-	 *
-	 * @return bool Returns true
+	 * @param Parser $parser
 	 */
-	public static function onParserFirstCallInit( Parser &$parser ) {
+	public static function onParserFirstCallInit( Parser $parser ) {
 		global $wgUseImageMagick, $wgScoreTrim;
 		if ( $wgScoreTrim === null ) {
 			// Default to if we use Image Magick, since it requires Image Magick.
 			$wgScoreTrim = $wgUseImageMagick;
 		}
 		$parser->setHook( 'score', 'Score::render' );
-		return true;
 	}
 
 	public static function onSoftwareInfo( array &$software ) {
