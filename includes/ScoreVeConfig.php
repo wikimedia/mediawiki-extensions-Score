dc3c6c223b898f2b1ca5f6975b4c43268192110f
Tim Starling <tstarling@wikimedia.org>
2023-05-08 14:47:15 +1000

    Migrate ScoreVeResourceLoaderModule to a virtual file callback
    Also replace array_map() with a loop, for readability.
    
    Depends-On: I97d61b5793159cea365740e0563f7b733e0f16de
    Bug: T47514
    Change-Id: I34174041354f6bacbaa3ae4f6f339b2bcfcd3aac

---
+++ includes/ScoreVeConfig.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MediaWiki\Extension\Score;
+
+use MediaWiki\MediaWikiServices;
+use MediaWiki\ResourceLoader as RL;
+
+class ScoreVeConfig {
+
+	/**
+	 * @param RL\Context $context
+	 * @return string JavaScript code
+	 */
+	public static function makeScript( RL\Context $context ) {
+		$utils = MediaWikiServices::getInstance()->getLanguageNameUtils();
+		$supportedLangs = [];
+		foreach ( Score::$supportedNoteLanguages as $lilyName => $code ) {
+			$supportedLangs[$lilyName] = $utils->getLanguageName( $code );
+		}
+		return 'mw.config.set('
+			. $context->encodeJson( [ 'wgScoreNoteLanguages' => $supportedLangs ] )
+			. ');';
+	}
+}
