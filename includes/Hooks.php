9d8015847165d463ea81d26fd3e6ca5a9befb984
Fomafix <fomafix@googlemail.com>
2024-06-10 05:47:49 +0000

    Use service MainConfig instead of global variable
    Change-Id: I9795d3a856e0de6ee0e613bffe751f7f1b0496c3

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,8 +2,10 @@
 
 namespace MediaWiki\Extension\Score;
 
+use MediaWiki\Config\Config;
 use MediaWiki\Hook\ParserFirstCallInitHook;
 use MediaWiki\Hook\SoftwareInfoHook;
+use MediaWiki\MainConfigNames;
 use Parser;
 use ValueFormatters\FormatterOptions;
 use Wikibase\Repo\Rdf\DedupeBag;
@@ -17,18 +19,26 @@ class Hooks implements
 	ParserFirstCallInitHook,
 	SoftwareInfoHook
 {
+	private Config $config;
+
+	public function __construct(
+		Config $config
+	) {
+		$this->config = $config;
+	}
+
 	/**
 	 * @param Parser $parser
 	 */
 	public function onParserFirstCallInit( $parser ) {
-		global $wgUseImageMagick, $wgScoreTrim, $wgScoreUseSvg;
+		global $wgScoreTrim, $wgScoreUseSvg;
 		if ( $wgScoreUseSvg ) {
 			// For SVG, always set true
 			$wgScoreTrim = true;
 		}
 		if ( $wgScoreTrim === null ) {
 			// Default to if we use Image Magick, since it requires Image Magick.
-			$wgScoreTrim = $wgUseImageMagick;
+			$wgScoreTrim = $this->config->get( MainConfigNames::UseImageMagick );
 		}
 		$parser->setHook( 'score', [ Score::class, 'render' ] );
 	}
