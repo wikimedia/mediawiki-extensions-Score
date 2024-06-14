6df91ec2283226925ec2934fd6e8f35038da734e
Ed Sanders <esanders@wikimedia.org>
2024-06-14 13:03:36 +0100

    build: Update eslint-config-wikimedia to 0.28.2
    Change-Id: I7dbe12711d6d365bfcf1e631f77e8e11a4cc815b

--- modules/ext.score.popup/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable quote-props, quotes */
-module.exports = {
-	// Ideally we would just use sourceType:module here, but that does not support ES5
-	"rules": {
-		"no-implicit-globals": "off"
-	}
-};
