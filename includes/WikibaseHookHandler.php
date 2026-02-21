f66df2020d7e983dcc7b786fefa88d726aaa0db7
Fomafix <fomafix@googlemail.com>
2026-02-21 22:01:42 +0000

    Implement Wikibase hook interfaces in WikibaseHookHandler
    Also inject Config and replace the global variables.
    
    The separate class is necessary to avoid a hard dependency on extension
    Wikibase.
    
    Change-Id: I2bb0cb379e9422918a76a170e7dcc01cd27bed7a

--- includes/WikibaseHookHandler.php
+++ includes/WikibaseHookHandler.php
@@ -2,7 +2,10 @@
 
 namespace MediaWiki\Extension\Score;
 
+use MediaWiki\Config\Config;
 use ValueFormatters\FormatterOptions;
+use Wikibase\Client\Hooks\WikibaseClientDataTypesHook;
+use Wikibase\Repo\Hooks\WikibaseRepoDataTypesHook;
 use Wikibase\Repo\Rdf\DedupeBag;
 use Wikibase\Repo\Rdf\EntityMentionListener;
 use Wikibase\Repo\Rdf\NullEntityRdfBuilder;
@@ -10,32 +13,40 @@ use Wikibase\Repo\Rdf\RdfVocabulary;
 use Wikibase\Repo\WikibaseRepo;
 use Wikimedia\Purtle\RdfWriter;
 
-class WikibaseHookHandler {
+class WikibaseHookHandler implements
+	WikibaseClientDataTypesHook,
+	WikibaseRepoDataTypesHook
+{
+	public function __construct(
+		private readonly Config $config,
+	) {
+	}
+
 	/**
 	 * Add Datatype "Musical notation" to the Wikibase Repository
 	 * @param array[] &$dataTypeDefinitions
 	 */
-	public static function onWikibaseRepoDataTypes( array &$dataTypeDefinitions ) {
-		global $wgMusicalNotationEnableWikibaseDataType;
-
+	public function onWikibaseRepoDataTypes( array &$dataTypeDefinitions ): void {
 		/**
 		 * Enable the datatype in Quibble (CI) contexts so that we can test the integration
 		 * of Score with Wikibase.
 		 */
-		if ( !$wgMusicalNotationEnableWikibaseDataType && !defined( 'MW_QUIBBLE_CI' ) ) {
+		if (
+			!$this->config->get( 'MusicalNotationEnableWikibaseDataType' ) &&
+			!defined( 'MW_QUIBBLE_CI' )
+		) {
 			return;
 		}
 
 		$dataTypeDefinitions['PT:musical-notation'] = [
 			'value-type' => 'string',
-			'validator-factory-callback' => static function () {
-				global $wgScoreMaxLength;
+			'validator-factory-callback' => function () {
 				// load validator builders
 				$factory = WikibaseRepo::getDefaultValidatorBuilders();
 				// initialize an array with string validators
 				// returns an array of validators
 				// that add basic string validation such as preventing empty strings
-				$validators = $factory->buildStringValidators( $wgScoreMaxLength );
+				$validators = $factory->buildStringValidators( $this->config->get( 'ScoreMaxLength' ) );
 				// $validators[] = new ScoreValidator();
 				// TODO: Take out the validation out of Score
 				return $validators;
@@ -60,14 +71,15 @@ class WikibaseHookHandler {
 	 * Add Datatype "Musical notation" to the Wikibase Client
 	 * @param array[] &$dataTypeDefinitions
 	 */
-	public static function onWikibaseClientDataTypes( array &$dataTypeDefinitions ) {
-		global $wgMusicalNotationEnableWikibaseDataType;
-
+	public function onWikibaseClientDataTypes( array &$dataTypeDefinitions ): void {
 		/**
 		 * Enable the datatype in Quibble (CI) contexts so that we can test the integration
 		 * of Score with Wikibase.
 		 */
-		if ( !$wgMusicalNotationEnableWikibaseDataType && !defined( 'MW_QUIBBLE_CI' ) ) {
+		if (
+			!$this->config->get( 'MusicalNotationEnableWikibaseDataType' ) &&
+			!defined( 'MW_QUIBBLE_CI' )
+		) {
 			return;
 		}
 		$dataTypeDefinitions['PT:musical-notation'] = [
