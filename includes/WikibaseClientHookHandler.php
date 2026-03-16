a63b27edea8e2d2a381648eebcba5fa885b9e229
Fomafix <fomafix@googlemail.com>
2026-03-16 11:17:02 +0000

    Implement Wikibase hook interfaces
    The separate classes for WikibaseClient and WikibaseRepo are necessary
    to avoid a hard dependency on extension WikibaseRepo.
    
    Also inject Config and replace the global variables.
    
    Change-Id: Ic55fcdb390707e6bc5c1ba459f21151628230ba7

---
+++ includes/WikibaseClientHookHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MediaWiki\Extension\Score;
+
+use MediaWiki\Config\Config;
+use ValueFormatters\FormatterOptions;
+use Wikibase\Client\Hooks\WikibaseClientDataTypesHook;
+
+class WikibaseClientHookHandler implements
+	WikibaseClientDataTypesHook
+{
+	public function __construct(
+		private readonly Config $config,
+	) {
+	}
+
+	/**
+	 * Add Datatype "Musical notation" to the Wikibase Client
+	 * @param array[] &$dataTypeDefinitions
+	 */
+	public function onWikibaseClientDataTypes( array &$dataTypeDefinitions ): void {
+		/**
+		 * Enable the datatype in Quibble (CI) contexts so that we can test the integration
+		 * of Score with Wikibase.
+		 */
+		if (
+			!$this->config->get( 'MusicalNotationEnableWikibaseDataType' ) &&
+			!defined( 'MW_QUIBBLE_CI' )
+		) {
+			return;
+		}
+		$dataTypeDefinitions['PT:musical-notation'] = [
+			'value-type' => 'string',
+			'formatter-factory-callback' => static function ( $format, FormatterOptions $options ) {
+				return new ScoreFormatter( $format );
+			},
+		];
+	}
+
+}
